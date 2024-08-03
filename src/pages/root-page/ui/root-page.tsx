9a0d55b2f59b328abc91115c971e4c1240d95b6e
uicode <uicodee@gmail.com>
2024-08-03 13:20:27 +0500

    init

--- src/pages/root-page/ui/root-page.tsx
+++ src/pages/root-page/ui/root-page.tsx
@@ -28,7 +28,8 @@ export const RootPage = () => {
                 {tabs.map((tab, index) => (
                     <Tabbar.Item
                         key={index}
-                        text={<p className="font-unbounded font-normal text-xs">{tab.title}</p>}
+                        // text={<p className="font-unbounded font-normal text-xs">{tab.title}</p>}
+                        text={tab.title}
                         selected={location.pathname === tab.href}
                         onClick={() => navigate(tab.href)}
                         style={{
